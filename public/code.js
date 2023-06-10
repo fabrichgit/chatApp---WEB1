10672baf64b76dc40b1af00584bd97f4a389961f
fabrich <fabrichjean@gmail.com>
2023-06-10 23:55:52 +0100

    chatapp

--- public/code.js
+++ public/code.js
@@ -1,82 +1,231 @@
-(function (){
+(function () {
     const app = document.querySelector(".app");
     const socket = io();
 
-    let uname;
-    app.querySelector(".join-screen #join-user").addEventListener("click",function(){
-        let username = app.querySelector(".join-screen #username").value;
-        if(username.length == 0){
-            return;
+    let username;
+    let room;
+
+    const btn_log = document.getElementById("form-login");
+
+
+    var counting = 0;
+
+
+    btn_log.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+
+        username = app.querySelector(".join-screen #pseudo").value;
+        room = app.querySelector(".join-screen #room").value;
+
+        if (username.trim() != "" || room.trim() != "") {
+
+            app.querySelector(".join-screen").classList.add("active");
+            app.querySelector(".chat-screen").classList.remove("active");
+
+            socket.emit("newuser", {
+                pseudo: username,
+                receiver: room
+            });
+
+        } else {
+            alert("Veuillez remplir orrectement tout les champs")
         }
-        socket.emit("newuser",username);
-        uname = username;
-        app.querySelector(".join-screen").classList.remove("active");
-        app.querySelector(".chat-screen").classList.add("active");
 
     });
-    app.querySelector(".chat-screen #send-message").addEventListener("click",function(){
+
+
+    app.querySelector(".chat-screen #send-message").addEventListener("click", function () {
         let message = app.querySelector(".chat-screen #message-input").value;
-        if(message.length == 0){
+        if (message.length == 0) {
             return;
         }
 
-        renderMessage("my",{
-            username:uname,
-            text:message
+        renderMessage("text", "my", {
+            pseudo: username,
+            text: message
         });
 
-        socket.emit("chat",{
-            username:uname,
-            text:message
+        socket.emit("chat", {
+            pseudo: username,
+            text: message,
+            receiver: room
         });
+
         app.querySelector(".chat-screen #message-input").value = "";
 
     });
-     app.querySelector(".chat-screen #exit-chat").addEventListener("click",function(){
-        socket.emit("exituser",uname);
+
+    app.querySelector(".chat-screen #darkLight").addEventListener("click", () => {
+        toogle_Light_Dark();
+    });
+
+
+
+    app.querySelector(".chat-screen label").addEventListener("click", () => {
+        let imgSelector = document.getElementById("image-input");
+
+        imgSelector.onchange = () => {
+            let reader = new FileReader();
+            reader.readAsDataURL(imgSelector.files[0]);
+
+            reader.addEventListener("load", () => {
+                let source = reader.result;
+                let imgUploaded = `
+                <div class="content-message myImage">
+                    <div class="name-img bgc-ownMp">
+                        <div class="name">You</div>
+                        <a href="${source}" title="watch it">
+                            <img src="${source}" />
+                        </a>
+                        <div class="name">${getTimeStamp()}</div>
+                    </div>
+                </div>`;
+                let imgRending = `
+                <div class="content-message otherImage">
+                    <div class="name-img bgc-otherMp">
+                        <div class="name">${username}</div>
+                        <a href="${source}" title="watch it">
+                            <img src="${source}" />
+                        </a>
+                        <div class="name">${getTimeStamp()}</div>
+                    </div>
+                </div>
+                `
+                renderMessage("image", "my", imgUploaded);
+
+                socket.emit("upload-img", {
+                    pseudo: username,
+                    content: imgRending,
+                    receiver: room
+                });
+            })
+
+        }
+    })
+
+
+    app.querySelector(".chat-screen #exit-chat").addEventListener("click", function () {
+        socket.emit("exituser", {
+            pseudo: username,
+            receiver: room
+        });
         window.location.href = window.location.href;
-     });
+    });
 
-    socket.on("update",function(update){
-        renderMessage("update",update);
-    })
-    socket.on("chat",function(message){
-        renderMessage("other",message);
+    socket.on("update", function (update) {
+        if (update.receiver != room) {
+            return;
+        } else {
+            renderMessage("text", "update", update.text);
+            return;
+        }
     })
 
-    function renderMessage(type,message){
-        let messageContainer = app.querySelector(".chat-screen .messages");
-        if(type == "my"){
+    socket.on("add-online", function (name) {
+        if (name.receiver != room) {
+            return;
+        } else {
             let el = document.createElement("div");
-            el.setAttribute("class","message my-message");
-            el.innerHTML = `
-            <div>
-            <div class="name">You</div>
-            <div class="text">${message.text}</div>
+            let listOnline = document.querySelector(".block1");
+
+            el.innerText = name.user;
+
+            listOnline.appendChild(el);
+            el.setAttribute("id", name.user);
+
+            el.classList.add("userComing");
+
+            return;
+        }
+    })
+
+    socket.on("chat", function (message) {
+        if (message.receiver != room) {
+            return;
+        } else {
+            renderMessage("text", "other", message);
+        }
+    })
+
+    socket.on("upload-img", (data) => {
+        if (data.receiver != room) {
+            return;
+        } else {
+            renderMessage("image", "other", data.content);
+        }
+    })
+
+
+    socket.on("offline", (data) => {
+        if (data.receiver != room) {
+            return;
+        } else {
+            document.getElementById(data.user).style.display = "none";
+        }
+    })
+
+    function renderMessage(type, owner, content) {
+        let messageContainer = app.querySelector(".chat-screen .block2");
+        if (type == "text" && owner == "my") {
+            messageContainer.innerHTML += `
+            <div class="content-message flex-end smsComing">
+                <div class="forText bgc-ownMp">
+                    <div class="name">You</div>
+                    <div class="text">${content.text}</div>
+                    <div class="name">${getTimeStamp()}</div>
+                </div>
             </div>
             `;
 
-            messageContainer.appendChild(el);
-
-        }else if(type == "other"){
-            let el = document.createElement("div");
-            el.setAttribute("class","message other-message");
-            el.innerHTML =`
-            <div>
-            <div class="name">${message.username}</div>
-            <div class="text">${message.text}</div>
+        } else if (type == "text" && owner == "other") {
+            messageContainer.innerHTML += `
+            <div class="content-message flex-start smsComing">
+                <div class="forText bgc-otherMp">
+                    <div class="name">${content.pseudo}</div>
+                    <div class="text">${content.text}</div>
+                    <div class="name">${getTimeStamp()}</div>
+                </div>
             </div>
             `;
 
-            messageContainer.appendChild(el);
-        }else if(type == "update"){
-            let el = document.createElement("div");
-            el.setAttribute("class","update");
-            el.innerText = message;
-            
-            messageContainer.appendChild(el);
+        } else if (owner == "update") {
+            let el1 = document.createElement("div");
+            el1.innerText = content;
 
+            messageContainer.appendChild(el);
+            el1.setAttribute("class", "update");
+
+        } else if (type == "image" && owner == "my") {
+            messageContainer.innerHTML += content;
+        } else if (type == "image" && owner == "other") {
+            messageContainer.innerHTML += content;
         }
         messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
     }
+
+
+    function getTimeStamp() {
+        let date = new Date();
+
+        return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} - ${date.getHours()}:${date.getMinutes()}`
+    }
+
+
+    function toogle_Light_Dark() {
+
+        document.getElementById("wallpaper").classList.toggle("negative");
+        document.getElementById("user-input").classList.toggle("negative");
+
+
+        if (counting % 2 == 0) {
+            document.getElementById("dark-light").setAttribute("src", "./img/8665313_cloud_sun_icon.png");
+        } else {
+            document.getElementById("dark-light").setAttribute("src", "./img/8541704_cloud_moon_icon.png");
+        }
+
+        counting++;
+    }
+
+
 })();
--- server.js
+++ server.js
@@ -6,21 +6,45 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname+"/public")));
 
-io.on("connection", function(socket){
-    socket.on("newuser",function(username){
-        socket.broadcast.emit("update",username +" "+ "joined the conversation");
+app.use(express.static(path.join(__dirname + "/public")));
+
+io.on("connection", function (socket) {
+    socket.on("newuser", function (user) {
+
+        socket.broadcast.emit("update", {
+            text: `${user.pseudo} is here`,
+            receiver: user.receiver
+        });
+
+        socket.broadcast.emit("add-online", {
+            receiver: user.receiver,
+            user: user.pseudo
+        });
+
     });
-    socket.on("exituser",function(username){
-        socket.broadcast.emit("update",username +" "+ "left the conversation");
+
+    socket.on("exituser", function (user) {
+        socket.broadcast.emit("update", {
+            text: `${user.pseudo} is gone`,
+            receiver: user.receiver
+        });
+
+        socket.broadcast.emit("offline", {
+            user: user.pseudo,
+            receiver: user.receiver
+        })
     });
-    socket.on("chat",function(message){
-        socket.broadcast.emit("chat",message);
+
+    socket.on("chat", function (message) {
+        socket.broadcast.emit("chat", message);
     });
-    
+
+    socket.on("upload-img", function (data) {
+        socket.broadcast.emit("upload-img", data);
+    })
+
 });
 
 
-server.listen(5000);
-
+server.listen(3000);
